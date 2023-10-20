d28a3cc5b361a85a0eac540fbe41b1457376c0bc
6godpro <erickson5115@gmail.com>
2023-10-20 13:36:19 +0100

    initial commit

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+    This module contains a class called Server.
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the indexes corresponding to the pagination parameters."""
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page."""
+        assert type(page) is int
+        assert type(page_size) is int
+        assert page > 0
+        assert page_size > 0
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        if not Server.ranges(start, end, len(dataset)):
+            return []
+        return dataset[start:end]
+
+    def get_hyper(self, page: int, page_size: int) -> dict:
+        """Returns a page with more details."""
+        data = self.get_page(page, page_size)
+        data_size = len(self.dataset())
+        total_pages = data_size // page_size
+        total_pages = total_pages if total_pages % 2 else total_pages + 1
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': None if page + 1 > total_pages else page + 1,
+            'prev_page': None if page <= 1 else page - 1,
+            'total_pages': total_pages
+        }
+
+    @staticmethod
+    def ranges(start: int, end: int, range: int) -> bool:
+        """Checks to see if start and end are within range."""
+        if start < range and end <= range:
+            return True
+        return False
